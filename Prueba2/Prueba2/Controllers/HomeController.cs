d7017e56be1bf7cccaf8f65f4c9dd424169978a4
cristiam <stevenflores88@hotmail.com>
2015-05-09 10:34:01 -0600

    Cristiam
    ahi va un ejemplo de como manejar el mvc solo falta meter la plantilla
    estoy con eso

---
+++ Prueba2/Prueba2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Prueba2.Models;
+
+namespace Prueba2.Controllers
+{
+    public class HomeController : Controller
+    {
+        Prueba p = new Prueba();
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            int n = p.probar();
+            ViewBag.Nombre = "emma luis cristiam";
+            ViewBag.Numero = n;
+
+            return View();
+        }
+
+    }
+}
---
+++ Prueba2/Prueba2/Models/Prueba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueba2.Models
+{
+    public class Prueba
+    {
+        public int probar()
+        {
+            return 5;
+        }
+    }
+    
+}
