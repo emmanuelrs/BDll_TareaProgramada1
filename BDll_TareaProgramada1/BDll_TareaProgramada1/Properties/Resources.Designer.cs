81a895445e84d1a284266f086bd670aa932f582c
Emmanuel Rosales <emmanuelrs@gmail.com>
2015-05-09 20:17:41 -0600

    update

--- BDll_TareaProgramada1/BDll_TareaProgramada1/Properties/Resources.Designer.cs
+++ BDll_TareaProgramada1/BDll_TareaProgramada1/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace BDll_TareaProgramada1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bgIndex1 {
+            get {
+                object obj = ResourceManager.GetObject("bgIndex1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap bglogin {
             get {
                 object obj = ResourceManager.GetObject("bglogin", resourceCulture);
